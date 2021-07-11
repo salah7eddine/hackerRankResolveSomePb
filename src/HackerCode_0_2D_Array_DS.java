69a89c81c569492c95feb39cba6856eb0b2361e6
Abousaid Salah-Eddine <abousaid7salah@gmail.com>
2021-07-11 21:57:04 +0100

    First Pb, 2D Array - DS

---
+++ src/HackerCode_0_2D_Array_DS.java
@@ -0,0 +1,92 @@
+import java.io.*;
+        import java.math.*;
+        import java.security.*;
+        import java.text.*;
+        import java.util.*;
+        import java.util.concurrent.*;
+        import java.util.function.*;
+        import java.util.regex.*;
+        import java.util.stream.*;
+        import static java.util.stream.Collectors.joining;
+        import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'hourglassSum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int hourglassSum(List<List<Integer>> arr) {
+        // Write your code here
+
+           /*int rows = arr.length;
+        int columns = arr[0].length;
+
+        //int max_hourglass_sum = Integer.MIN_VALUE;
+        int max_hourglass_sum = -63;
+
+        for (int i = 0; i < rows - 2; i++) {
+            for (int j = 0; j < columns - 2; j++) {
+                int current_hourglass_sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1]
+                        + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+
+                max_hourglass_sum = Math.max(max_hourglass_sum, current_hourglass_sum);
+            }
+        }
+
+        return max_hourglass_sum;*/
+
+
+        int rows = arr.toArray().length;
+        int columns = arr.get(0).toArray().length;
+
+        int max_hourglass_sum = -63;
+
+        for (int i = 0; i < rows - 2; i++) {
+            for (int j = 0; j < columns - 2; j++) {
+                int current_hourglass_sum = arr.get(i).get(j) + arr.get(i).get(j+1) + arr.get(i).get(j+2) + arr.get(i+1).get(j+1)
+                        + arr.get(i+2).get(j) + arr.get(i+2).get(j+1) + arr.get(i+2).get(j+2);
+
+                max_hourglass_sum = Math.max(max_hourglass_sum, current_hourglass_sum);
+            }
+        }
+
+
+        return max_hourglass_sum;
+
+    }
+
+}
+
+public class HackerCode_0_2D_Array_DS {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, 6).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.hourglassSum(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
